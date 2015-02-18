ca64109324493f03161fba05dc74b7e3ce52af08
Romain Rui <romain.rui10@gmail.com>
2015-02-18 21:01:41 +0100

    structs

---
+++ had/structs.swift
@@ -0,0 +1,15 @@
+//
+//  StaticData.swift
+//  had
+//
+//  Created by chrisdegas on 17/02/2015.
+//  Copyright (c) 2015 had. All rights reserved.
+//
+
+import UIKit
+
+struct URLS {
+    
+    static var urlUser="http://151.80.128.136:3000/user/"
+    static var urlPlace="http://151.80.128.136:3000/place/"
+}
